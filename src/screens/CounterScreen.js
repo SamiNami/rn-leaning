69665621b495cca8b1d4d9775d6001456a7affb7
sami peralahti <sami.peralahti@vainu.io>
2019-08-07 17:45:16 +0300

    useReducer practice

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'change_count':
+            return { ...state, counter: action.payload };
+        default:
+            return state;
+    }
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+    const { counter } = state;
 
     return (
         <View>
             <Button
                 title="Increase"
                 onPress={() => {
-                    setCounter(counter + 1);
+                    dispatch({
+                        type: 'change_count',
+                        payload: counter + 1
+                    });
                 }}
             />
             <Button
                 title="Decrease"
                 onPress={() => {
-                    setCounter(counter - 1);
+                    dispatch({
+                        type: 'change_count',
+                        payload: counter - 1
+                    });
                 }}
             />
             <Text>Current count: {counter}</Text>
