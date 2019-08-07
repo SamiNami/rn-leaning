fa0880d65587f660254a54b4450c70276e11da04
sami peralahti <sami.peralahti@vainu.io>
2019-08-07 13:34:49 +0300

    simple color demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,6 +21,10 @@ const HomeScreen = ({ navigation }) => {
                 title="Go to counter screen"
                 onPress={() => navigation.navigate('Counter')}
             />
+            <Button
+                title="Go to color demo"
+                onPress={() => navigation.navigate('Color')}
+            />
         </View>
     );
 };
