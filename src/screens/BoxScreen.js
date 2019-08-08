30a5bcace30647f3b95190f2205e746e01611ee3
sami peralahti <sami.peralahti@vainu.io>
2019-08-08 14:53:18 +0300

    practising styling

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,20 +4,37 @@ import { View, StyleSheet , Text } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}> BoxScreen </Text>
+            <View style={styles.textStyle1} />
+            <View style={styles.textStyle2} />
+            <View style={styles.textStyle3} />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     viewStyle: {
+        height: 200,
         borderWidth: 3,
         borderColor: 'black',
+        // alignItems: 'flex-end',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
-    textStyle: {
-        borderWidth: 1,
-        borderColor: 'red',
-        margin: 20
+    textStyle1: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'red'
+    },
+    textStyle2: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'green',
+        alignSelf: 'flex-end'
+    },
+    textStyle3: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'blue'
     }
 });
 export default BoxScreen;
